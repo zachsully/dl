2c37a4b4ea42847133cbdee1af98581cb9a61fc4
Zach Sullivan <zachsully@gmail.com>
2017-08-16 18:01:54 -0700

    successful evaluation of fstProj and sndProj copatterns

--- src/DualLanguage.hs
+++ src/DualLanguage.hs
@@ -69,13 +69,20 @@ data Pattern where
 {- NOTE: we often use 'q' for a copattern variables -}
 data CoPattern where
   QWild :: CoPattern
+  QBox  :: CoPattern
   QDest :: Symbol -> CoPattern -> CoPattern
+  QPat  :: CoPattern -> Pattern -> CoPattern
   deriving (Eq,Show)
 
 {- Values are always positive data types. Also not that the are in weak head
    normal form, implying call-by-name -}
 data Value where
   VCons :: Symbol -> [Term] -> Value
+  deriving (Eq,Show)
+
+data CoValue where
+  VDest :: Symbol -> Term -> CoValue
+  VCoCase :: [(CoPattern,Term)] -> CoValue
   deriving (Eq,Show)
 
 {- Symbols are introduced in typing declarations -}
@@ -185,33 +192,121 @@ infer s c (Case t _) =
   let ty = infer s c t
   in undefined
 
+infer _ _ (Dest _ _) = undefined
+infer _ _ (CoCase _) = undefined
+
+
 -----------
 -- check --
 -----------
 check :: Sig -> Ctx -> Term -> Type -> Bool
 check _ _ (Cons _ _) _  = undefined
 check _ _ (Case _ _) _  = undefined
-
+check _ _ (Dest _ _) _  = undefined
+check _ _ (CoCase _) _  = undefined
 
 --------------------------------------------------------------------------------
 --                              Evaluation                                    --
 --------------------------------------------------------------------------------
 
-type EvalCtx = [(Variable,Term)]
+type Env = [(Variable,Term)]
 
-eval :: EvalCtx -> Term -> Value
-eval _ (Cons _ _) = undefined
-eval _ (Case _ _) = undefined
+{- An evaluation context for CoPatterns to match on -}
+data QCtx where
+  Box        :: QCtx
+  Destructor :: QCtx -> Pattern -> QCtx
+  Destructee :: Symbol -> QCtx -> QCtx
+
+evalClosed :: Term -> Either CoValue Value
+evalClosed = eval [] Box
+
+eval :: Env -> QCtx -> Term -> Either CoValue Value
+eval env qc (Var v) =
+  case lookup v env of
+    Just t -> eval env qc t
+    Nothing -> error $ "unbound variable" ++ show v
+
+eval _ _ (Cons k ts) = Right (VCons k ts)
+
+eval env qc (Case t alts) =
+  case eval env qc t of
+    Right (VCons k ts) -> tryAlts (Cons k ts) alts
+    _ -> error "case only operates on constructed values"
+  where tryAlts :: Term -> [(Pattern,Term)] -> Either CoValue Value
+        tryAlts _ [] = error "no matching alternative"
+        tryAlts t' ((p,t''):alts') =
+          case matchPattern t' p of
+            Just subs -> eval (subs++env) qc t''
+            Nothing -> tryAlts t' alts'
+
+        matchPattern :: Term -> Pattern -> Maybe [(Variable,Term)]
+        matchPattern t PWild = Just []
+        matchPattern t (PVar v) = Just [(v,t)]
+        matchPattern (Cons k ts) (PCons k' ps) =
+          case k == k' of
+            True -> let subs = map (\(t,p) -> matchPattern t p) (zip ts ps)
+                    in foldr (\msub acc ->
+                               case (msub,acc) of
+                                 (_,Nothing) -> Nothing
+                                 (Nothing,_) -> Nothing
+                                 (Just as,Just bs) -> Just (as ++ bs)
+                             ) (Just []) subs
+            False -> Nothing
+        matchPattern _ _ = Nothing
+
+
+
+eval env qc (Dest s t) =
+  let qc' = Destructee s qc in
+  case eval env qc'  t of
+    Left (VDest s' t') -> undefined
+    Left (VCoCase coalts) -> tryCoAlts qc' coalts
+    _ -> error "destructors must be applied to cocase"
+  where tryCoAlts :: QCtx -> [(CoPattern,Term)] -> Either CoValue Value
+        tryCoAlts _ [] = error "no copattern match"
+        tryCoAlts qc ((q,t):coalts') =
+          case matchCoPattern qc  q of
+            True -> eval env qc t
+            False -> tryCoAlts qc coalts'
+
+        matchCoPattern :: QCtx -> CoPattern -> Bool
+        matchCoPattern _   QWild = True
+        matchCoPattern Box QBox  = True
+        matchCoPattern (Destructee sym qctx) (QDest sym' q) =
+          case sym == sym' of
+            True -> matchCoPattern qctx q
+            False -> False
+        matchCoPattern _ _ = False
+
+eval _ _ (CoCase coalts) = Left (VCoCase coalts)
 
 
 --------------------------------------------------------------------------------
 --                               Examples                                     --
 --------------------------------------------------------------------------------
 
+unit :: Term
+unit = Cons (Symbol "()") []
+
 pair1 :: Term
-pair1 = Cons (Symbol "mkPair") [undefined,undefined]
+pair1 = Cons (Symbol "mkPair") [Cons (Symbol "mkPair") [unit,unit],unit]
 
 pair2 :: Term
 pair2 = Case pair1 [(PCons (Symbol "mkPair")
                            [PVar (Variable "x")
                            ,PVar (Variable "y")], Var (Variable "x"))]
+
+lam :: Term
+lam = CoCase [(QPat QWild (PVar (Variable "x"))
+              ,Cons (Symbol "mkPair") [Var (Variable "x")
+                                      ,Var (Variable "x")])]
+
+negPair :: Term
+negPair = CoCase [(QDest (Symbol "fst") QWild, unit)
+                 ,(QDest (Symbol "snd") QWild, pair1)]
+
+fstProj :: Term
+fstProj = Dest (Symbol "fst") negPair
+
+sndProj :: Term
+sndProj = Dest (Symbol "snd") negPair
