36ccb0dcb174411da2870e96dbb48a0a527f9874
Zach Sullivan <zachsully@gmail.com>
2018-02-17 12:27:17 -0800

    update sample ocaml output to use exceptions instead of options so that type inference says the type we expect.

--- examples/target/foo.ml
+++ examples/target/foo.ml
@@ -2,30 +2,29 @@ open Lazy
 
 type ('a , 'b) mkNegPair  =  { getFst : 'a lazy_t option; getSnd : 'b lazy_t option;  }
 
-let setfst mcd br =
-  match mcd with
-    None -> Some { getFst = Some br; getSnd = None; }
-  | Some cd -> Some { getFst = Some br; getSnd = cd.getSnd; };;
-let obsfst mcd =
-  match mcd with
-    None -> failwith "unmatched"
-  | Some cd ->
-     match cd.getFst with
-       None -> failwith "unmatched"
-     | Some br -> force br;;
-let setsnd mcd br =
-  match mcd with
-    None -> Some { getFst = None; getSnd = Some br; }
-  | Some cd -> Some { getFst = cd.getFst; getSnd = Some br; };;
-let obssnd mcd =
-  match mcd with
-    None -> failwith "unmatched"
-  | Some cd ->
-     match cd.getSnd with
-       None -> failwith "unmatched"
-     | Some br -> force br;;
+exception Unmatched
+let bot = lazy (raise Unmatched);;
 
-let prog = (obsfst (setfst (setsnd None (lazy 0)) (lazy 42)));;
+let setfst cd br =
+  try let cdn = { getFst = Some br; getSnd = (force cd).getSnd; }
+      in lazy cdn with
+    Unmatched -> lazy { getFst = Some br; getSnd = None; }
+let obsfst cd =
+  try (match (force cd).getFst with
+         None -> force bot
+       | Some br -> force br) with
+    Unmatched -> raise Unmatched;;
+let setsnd cd br =
+  try let cdn = { getFst = (force cd).getFst; getSnd = Some br; }
+      in lazy cdn with
+    Unmatched -> lazy { getFst = None; getSnd = Some br; }
+let obssnd cd =
+  try (match (force cd).getSnd with
+         None -> force bot
+       | Some br -> force br) with
+    Unmatched -> raise Unmatched;;
+
+let prog = (obssnd (setfst bot (lazy 42)));;
 
 print_int prog;;
 print_newline ();;
