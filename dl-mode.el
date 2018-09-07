191d8e0f57726a18f01a6540d588cbdb05ef9b56
Zach Sullivan <zachsully@gmail.com>
2018-09-06 18:53:43 -0700

    work on emacs mode

--- dl-mode.el
+++ dl-mode.el
@@ -1,26 +1,60 @@
-(defvar dl-mode-syntax-table nil "syntax table for dl-mode.")
-(setq dual-lang-mode-syntax-table
-      (let ((st (make-syntax-table)))
-	(modify-syntax-entry ?\- ". 12b" st)
-	(modify-syntax-entry ?\n "> b" st)
-	st))
+(defgroup dl-faces nil
+  "Fonts and colours for DL mode.
 
-;; Definition
-(define-derived-mode dl-mode fundamental-mode
-  ;; use warning-face for postive syntax and variable-name-face for positive
-  ;;   variables
-  ;; use type-face for negative syntax and constant-face for negative variables
-  (setq font-lock-defaults
-  	'((("codata\s"              . font-lock-warning-face)
-	   ("cocase\s"              . font-lock-variable-name-face)
-	   ("data\s"                . font-lock-constant-face)
-	   ("case\s"                . font-lock-type-face)
-           ("\(fix\|in\|let\)\s"    . font-lock-string-face)
-	   )))
-  ;; (set-syntax-table dual-lang-mode-syntax-table)
-  (setq set-input-method "delimiters")
-  (setq mode-name "dl-mode"))
+ These colours are set up to highlight the duality of data and
+  codata types.")
 
-;; Loading
-(autoload 'dl-mode "dl-mode" nil t)
+(defface dl-codata-face
+  '((t (:inherit font-lock-warning-face)))
+  "Face highlighting codata keyword"
+  :group 'dl-faces)
+
+;; (defface dl-cocase-face
+;;   '((t (:inherit font-lock-variable-name-face)))
+;;   "Face highlighting cocase keyword"
+;;   :group 'dl-faces)
+
+;; (defface dl-keyword-face
+;;   '((t (:inherit font-lock-string-face))))
+
+
+(defconst dl-mode-font-lock-defaults
+  `(;; Codata stuff
+    (,(regexp-opt '("codata") 'words) . font-lock-warning-face)
+    (,(regexp-opt '("cocase") 'words) . font-lock-variable-name-face)
+
+    ;; Data stuff
+    (,(regexp-opt '("data") 'words) . font-lock-constant-face)
+    (,(regexp-opt '("case") 'words) . font-lock-type-face)
+
+    ;; Neutralish stuff
+    (,(regexp-opt '("fix" "in" "let") 'words) . font-lock-string-face)
+    (,(regexp-opt '("index") 'words) . font-lock-keyword-face)
+    (,(regexp-opt '("=>" "->" ":" "|" "," "#"
+                    "(" ")" "{" "}" "[" "]"))
+     . font-lock-preprocessor-face)
+    )
+  "Language keywords for DL modes")
+
+(defun dl-mode-syntax-table ()
+  "Syntax table for DL"
+  (let ((st (make-syntax-table)))
+    (modify-syntax-entry ?- ". 23b" st)
+    (modify-syntax-entry ?{ ". 1" st)
+    (modify-syntax-entry ?} ". 4" st)
+    (modify-syntax-entry ?\n ">" st)
+    st))
+
+(defun dl-mode ()
+  (interactive)
+  (kill-all-local-variables)
+  (set-syntax-table  (dl-mode-syntax-table))
+  (set (make-local-variable 'font-lock-defaults) `(,dl-mode-font-lock-defaults))
+  (setq major-mode 'dl-mode)
+  (setq mode-name "DL")
+  "Major mode for editing dual language files")
+
+(autoload 'dl-mode "DL" nil t)
 (add-to-list 'auto-mode-alist '("\\.dl$" . dl-mode))
+
+(provide 'dl-mode)
